a04828ebb7e8ce9697b326dcff0cf781c35d0efb
Oliver Steele <steele@osteele.com>
2017-12-19 08:51:50 -0500

    Apply PEP8 and PEP257 formatting rules

--- mbta_finder.py
+++ mbta_finder.py
@@ -2,9 +2,6 @@
 Geocoding and Web APIs Project Toolbox exercise
 
 Find the MBTA stops closest to a given location.
-
-Full instructions are at:
-https://sites.google.com/site/sd15spring/home/project-toolbox/geocoding-and-web-apis
 """
 
 from urllib.request import urlopen
@@ -20,16 +17,14 @@ MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
 # A little bit of scaffolding if you want to use it
 
 def get_json(url):
-    """
-    Given a properly formatted URL for a JSON web API request, return
+    """Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
     pass
 
 
 def get_lat_long(place_name):
-    """
-    Given a place name or address, return a (latitude, longitude) tuple
+    """Given a place name or address, return a (latitude, longitude) tuple
     with the coordinates of the given place.
 
     See https://developers.google.com/maps/documentation/geocoding/
@@ -39,8 +34,7 @@ def get_lat_long(place_name):
 
 
 def get_nearest_station(latitude, longitude):
-    """
-    Given latitude and longitude strings, return a (station_name, distance)
+    """Given latitude and longitude strings, return a (station_name, distance)
     tuple for the nearest MBTA station to the given coordinates.
 
     See http://realtime.mbta.com/Portal/Home/Documents for URL
@@ -50,8 +44,7 @@ def get_nearest_station(latitude, longitude):
 
 
 def find_stop_near(place_name):
-    """
-    Given a place name or address, print the nearest MBTA stop and the
+    """Given a place name or address, print the nearest MBTA stop and the
     distance from the given place to that stop.
     """
     pass
